ede47badd7099518873d6ce2342f91426a761ca2
PiisRational <gecho.gaecho@gmail.com>
2023-12-24 14:38:33 +0100

    add simple utilities for the clause db

--- src/clause db.zig
+++ src/clause db.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Clause = @import("clause.zig").Clause;
 const ClauseAlloc = @import("clause alloc.zig").ClauseAllocator;
+const Literal = @import("literal.zig").Literal;
 
 /// ClauseDb is the database for all clauses with at least 3 literals
 const ClauseDb = struct {
@@ -17,5 +18,18 @@ const ClauseDb = struct {
             .clause_alloc = ClauseAlloc.init(Allocator),
             .allocator = allocator,
         };
+    }
+
+    /// adds a clause containing `literals` to the clause database
+    pub fn addClause(self: *Self, literals: []Literal) void {
+        var clause = self.clause_alloc.alloc(literals.len);
+        @memcpy(clause.getLiterals(), literals);
+        self.clauses.append(clause);
+    }
+
+    /// removes `clause` from the clause database
+    pub fn removeClause(self: *Self, clause: *Clause) void {
+        self.clause_alloc.free(clause.*);
+        clause.* = self.clauses.pop();
     }
 };
