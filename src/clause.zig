21fd99ee495420d0683a76251759b3a319338e7e
PiisRational <gecho.gaecho@gmail.com>
2023-12-27 11:25:08 +0100

    update the clause

--- src/clause.zig
+++ src/clause.zig
@@ -5,6 +5,7 @@ const Literal = @import("literal.zig").Literal;
 const Variable = @import("variable.zig").Variable;
 const MemoryCell = @import("mem cell.zig").MemoryCell;
 const SatInstance = @import("sat instance.zig").SatInstance;
+const ClauseDb = @import("clause db.zig").ClauseDb;
 
 /// the clause struct
 ///
@@ -14,45 +15,68 @@ const SatInstance = @import("sat instance.zig").SatInstance;
 ///
 /// after that the literals are stored in order
 pub const Clause = struct {
-    cells: [*]MemoryCell,
+    index: usize,
     const Self = @This();
 
     /// sets the literals of
-    pub fn setLiterals(self: *Self, literals: []Literal) void {
-        @memcpy(self.getLiterals(), literals);
+    pub fn setLiterals(self: *Self, db: *ClauseDb, literals: []Literal) void {
+        @memcpy(self.getLiterals(db), literals);
     }
 
-    /// initializes a clause from the pointer to its header
-    pub fn fromHeader(header: *ClauseHeader) Clause {
+    /// initializes a clause from the index to its header
+    pub fn fromHeader(header_idx: usize) Clause {
         return Clause{
-            .cells = @ptrCast(header),
+            .index = header_idx,
         };
     }
 
     /// checks if this clause points to garbage in memory
-    pub fn isGarbage(self: Self) bool {
-        return self.cells[0].header.is_garbage;
+    pub fn isGarbage(self: Self, db: *ClauseDb) bool {
+        return db.*.memory[self.index].header.is_garbage;
     }
 
     /// getter for the amount of literals in this clause
-    pub fn getLength(self: Self) usize {
-        return self.cells[0].header.len;
+    pub fn getLength(self: Self, db: *ClauseDb) usize {
+        return db.*.memory[self.index].header.len;
     }
 
     /// getter for the literals contained in this clause as a slice
-    pub fn getLiterals(self: Self) []Literal {
-        return @ptrCast(self.cells[1 .. self.getLength() + 1]);
+    pub fn getLiterals(self: Self, db: *ClauseDb) []Literal {
+        return @ptrCast(db.memory[self.index + 1 .. self.index + self.getLength() + 1]);
     }
 
     /// checks that this clause is satisfied
     pub fn isSatisfied(self: Self, instance: *SatInstance) bool {
-        for (self.getLiterals()) |lit| {
+        for (self.getLiterals(instance.clauses)) |lit| {
             if (instance.isTrue(lit)) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    /// returns the reference to the memory behind this clause
+    pub fn getRef(self: Self, db: *ClauseDb) ClauseRef {
+        return ClauseRef{
+            .header = db.clauses.items[self.index],
+        };
+    }
+};
+
+pub const ClauseRef = struct {
+    header: [*]MemoryCell,
+
+    const Self = @This();
+
+    /// getter for the length of this clause
+    pub fn getLength(self: Self) usize {
+        return @intCast(self.header[0].header.len);
+    }
+
+    /// getter for the literals of this clause
+    pub fn getLiterals(self: Self) []Literal {
+        return @ptrCast(self.header[1 .. 1 + self.getLength()]);
     }
 
     pub fn format(
